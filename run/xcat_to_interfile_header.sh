da26a5f0885aec40cb33680f9017bbd16d5a9b8a
Alexander C Whitehead <alexandercwhitehead@gmail.com>
2019-02-26 12:02:23 +0000

    made changes suggested by elise added shebang added variables for all paths get existing file count usign command commented

--- run/xcat_to_interfile_header.sh
+++ run/xcat_to_interfile_header.sh
@@ -1,13 +1,17 @@
-EXISTINGFILECOUNT=2
+#!/bin/bash
 
-XCATPATH="/home/alexander/XCAT/"
-XCAT="dxcat2_linux_64bit"
-SAMPPAR="xcat.samp.par"
-PREFIX="prefix"
+#current number of files in directory
+EXISTINGFILECOUNT=$(ls | wc -l)
 
-ACTPREFIX=$PREFIX"_act_"
-ATNPREFIX=$PREFIX"_atn_"
+#user variables
+XCATPATH="/home/alexander/XCAT/" #path to xcat directory
+XCAT="dxcat2_linux_64bit" #xcat executable
+SAMPPARPATH="/home/alexander/Workspace/ALEXJAZZ008008_xcat_example/xcat_example/run/" #path to xcat config file
+SAMPPAR="xcat.samp.par" #xcat config file
+PREFIXPATH=$SAMPPARPATH #path to output
+PREFIX="prefix" #prefix for xcat output
 
+#generic parts of interfile header
 HEADERPART1="!INTERFILE  :=\nname of data file := "
 HEADERPART2=".bin\n!GENERAL DATA :=\n!GENERAL IMAGE DATA :=\n!type of data := PET\nimagedata byte order := LITTLEENDIAN\n!PET STUDY (General) :=\n!PET data type := Image\nprocess status := Reconstructed\n!number format := float\n!number of bytes per pixel := 4\nnumber of dimensions := 3\nmatrix axis label [1] := x\n!matrix size [1] := "
 HEADERPART3="\nscaling factor (mm/pixel) [1] := "
@@ -17,40 +21,49 @@ HEADERPART6="\nmatrix axis label [3] := z\n!matrix size [3] := "
 HEADERPART7="\nscaling factor (mm/pixel) [3] := "
 HEADERPART8="\nnumber of time frames := 1\n!END OF INTERFILE :="
 
+#aquires variables from xcat config file
 ARRAYSIZE=$(echo $(grep "array_size = " "./"$SAMPPAR) | cut -d' ' -f3)
 PIXELWIDTH=$(echo $(grep "pixel_width = " "./"$SAMPPAR) | cut -d' ' -f3)
 ENDSLICE=$(echo $(grep "endslice = " "./"$SAMPPAR) | cut -d' ' -f3)
 STARTSLICE=$(echo $(grep "startslice = " "./"$SAMPPAR) | cut -d' ' -f3)
 SLICEWIDTH=$(echo $(grep "slice_width = " "./"$SAMPPAR) | cut -d' ' -f3)
 
+#converts xcat config file variables which are in cm to mm for interfile header
 PIXELWIDTHMM=$(echo "$PIXELWIDTH*10" | bc -l)
 SLICEWIDTHMM=$(echo "$SLICEWIDTH*10" | bc -l)
 
+#number of slices
 MATRIXSIZE=$(echo "($ENDSLICE-$STARTSLICE)+1" | bc -l)
 
+#to be appended to file names
+ACTPREFIX=$PREFIX"_act_"
+ATNPREFIX=$PREFIX"_atn_"
+
+#constructs what is the same for every interfile header
 GENERALHEADERPART=$HEADERPART2$ARRAYSIZE$HEADERPART3$PIXELWIDTHMM$HEADERPART4$ARRAYSIZE$HEADERPART5$PIXELWIDTHMM$HEADERPART6$MATRIXSIZE$HEADERPART7$SLICEWIDTHMM$HEADERPART8
 
-DIR=$(pwd)"/"
-
+#moves to xcatpath
 cd $XCATPATH
 
-"./"$XCAT $DIR$SAMPPAR $DIR$PREFIX
+#runs xcat
+"./"$XCAT $SAMPPARPATH$SAMPPAR $PREFIXPATH$PREFIX
 
-cd $DIR
+#moves to sampparpath
+cd $SAMPPARPATH
 
-i=1
-END=$(echo $(echo "(($(ls | wc -l)-$EXISTINGFILECOUNT)-1)/2" | bc -l) | cut -d'.' -f1)
+i=1 #loop iterator
+END=$(echo $(echo "(($(ls | wc -l)-$EXISTINGFILECOUNT)-1)/2" | bc -l) | cut -d'.' -f1) #loop to
 
-while [ $i -le $END ]; do
-    echo $i
-    
+while [ $i -le $END ]
+do
+    #construct and output activity header
     FILE=$ACTPREFIX$i
+    echo -e $HEADERPART1$FILE$GENERALHEADERPART > $FILE".hv"
     
-    echo $HEADERPART1$FILE$GENERALHEADERPART > $FILE".hv"
-    
+    #construct and output attenuation header
     FILE=$ATNPREFIX$i
+    echo -e $HEADERPART1$FILE$GENERALHEADERPART > $FILE".hv"
     
-    echo $HEADERPART1$FILE$GENERALHEADERPART > $FILE".hv"
-    
+    #increment iterator
     i=$(($i+1))
 done
