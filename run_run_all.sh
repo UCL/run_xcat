6af155e4223b1995309fffd80813c31b18ce901d
ALEXJAZZ008008 <alexandercwhitehead@gmail.com>
2019-04-23 19:38:57 +0100

    added run script

---
+++ run_run_all.sh
@@ -0,0 +1,5 @@
+cd ../
+
+./run_xcat/run_all.sh ./run_xcat/run_xcat.sh ~/XCAT/dxcat2_linux_64bit ./run_xcat/run_lesion.sh /home/alex/Documents/SIRF-SuperBuild_install/bin/stir_math 
+
+cd ./run_xcat
