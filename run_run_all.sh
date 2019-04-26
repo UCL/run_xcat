874aee8a93cba2d3755c659e242dcca0ee0a7897
ALEXJAZZ008008 <alexandercwhitehead@gmail.com>
2019-04-26 13:41:05 +0100

    added foreword

--- run_run_all.sh
+++ run_run_all.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+#Copyright University College London 2019
+#Author: Alexander Whitehead, Institute of Nuclear Medicine, UCL
+#For internal research only.
+
 cd ../
 
 ./run_xcat/run_all.sh ./run_xcat/run_xcat.sh ~/XCAT/dxcat2_linux_64bit ./run_xcat/run_lesion.sh /home/alex/Documents/SIRF-SuperBuild_install/bin/stir_math 
